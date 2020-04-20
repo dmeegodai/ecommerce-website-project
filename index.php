b97c39b98971e109460d4b89dd49433c1fe7339c
Dilan Meegodavithana <63978309+dmeegodai@users.noreply.github.com>
2020-04-20 17:55:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>Nikkie's Craft Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href=".//styles/style.css">
+</head>
+
+<body>
+    <div id="top"><!-- Top Begining -->
+        <div class="container"><!--Container Begining -->
+            <div class="col-md-6 offer"><!--col-md-6 offer Begining -->
+                <a href="#" class="btn btn-success btn-sm">Test Button</a>
+                <a href="checkout.php">Shoping Cart Total Price Rs1500 </a>
+            </div><!--col-md-6 offer Ending -->
+            <div class="col-md-6"><!--col-md-6 Begining -->
+                <ul class="menu"><!--menu Begining -->
+                    <li>
+                        <a href="curomer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">To Cart</a>
+                    </li>
+                </ul><!--menu Ending -->
+            </div><!--col-md-6 Ending -->
+        </div><!--Container Ending -->
+    </div><!-- Top Ending --> 
+
+    <script src="js/jquery-331.min.js"> </script>
+    <script src="js/bootstrap-337.min.js"></script>
+
+</body>
+
+</html>
